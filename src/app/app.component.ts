e3de7c39220791f2ea7045c1460eed513fe2d37f
Santiago Paz <santiago.paz.1992@gmail.com>
2023-08-09 11:15:42 -0300

    Add Card component and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Card } from './interfaces/card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CursoDeAngular';
+  nombre = 'Santiago';
+  cards: Array<Card> = [
+    {
+      title: 'Titulo 1',
+      secondaryTitle: 'Titulo Secundario 1',
+      resourceUrl: '/assets/card_img_placeholder.svg',
+      supportingText: 'Texto de Soporte 1',
+    },
+    {
+      title: 'Titulo 2',
+      secondaryTitle: 'Titulo Secundario 2',
+      resourceUrl: '/assets/card_img_placeholder.svg',
+      supportingText: 'Texto de Soporte 2',
+    },
+    {
+      title: 'Titulo 3',
+      secondaryTitle: 'Titulo Secundario 3',
+      resourceUrl: '/assets/card_img_placeholder.svg',
+      supportingText: 'Texto de Soporte 3',
+    },
+  ];
+
+  onButtonClick() {
+    this.nombre = 'Ana';
+  }
 }
---
+++ src/app/interfaces/card.ts
@@ -0,0 +1,6 @@
+export interface Card {
+    title: string;
+    secondaryTitle: string;
+    supportingText: string;
+    resourceUrl: string;
+}
