7224bf47b73ff0e6010a44f223dc954d0ce29cbc
Santiago Paz <santiago.paz.1992@gmail.com>
2023-08-10 23:09:23 -0300

    Comment app spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+/* import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -27,3 +27,4 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('.content span')?.textContent).toContain('CursoDeAngular app is running!');
   });
 });
+ */
