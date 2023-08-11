f107781ee461d4a6a7c2e63acd42836dc852b418
Santiago Paz <santiago.paz.1992@gmail.com>
2023-08-10 22:33:56 -0300

    Modify Card input to use Card interface

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Card } from '../interfaces/card';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +8,5 @@ import { Component, Input } from '@angular/core';
 })
 export class CardComponent {
   @Input()
-  title: string = "";
+  card!: Card; 
 }
