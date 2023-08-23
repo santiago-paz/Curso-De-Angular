2237e2b93d1aaada57055c94710df32857b17ed0
Santiago Paz <santiago.paz.1992@gmail.com>
2023-08-23 08:03:06 -0300

    Add card card view

--- src/app/views/add-card-view/add-card-view.component.ts
+++ src/app/views/add-card-view/add-card-view.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Card } from 'src/app/interfaces/card';
 
 @Component({
   selector: 'app-add-card-view',
   templateUrl: './add-card-view.component.html',
-  styleUrls: ['./add-card-view.component.css']
+  styleUrls: ['./add-card-view.component.css'],
 })
 export class AddCardViewComponent {
-
+  constructor(private router: Router) {}
+  onAddCardButtonClick(card: Card) {
+    this.router.navigate([''], { queryParams: { title: card.title } });
+  }
 }
